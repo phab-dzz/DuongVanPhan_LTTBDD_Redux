f11a629bc33463e4e7ebf047254fa0b32eb47b5e
H51M14\Student <Student@gmail.com>
2024-10-30 08:07:17 +0700

    Saga Redux

---
+++ DuongVanPhan_redux_TH/App.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { View, Text, Button, FlatList, TextInput, ActivityIndicator,ScrollView } from 'react-native';
+import store from './store';
+import { fetchDataRequest, createDataRequest, updateDataRequest, deleteDataRequest } from './actions';
+
+const DataComponent = () => {
+  const dispatch = useDispatch();
+  const { loading, data, error } = useSelector(state => state);
+  const [title, setTitle] = useState('');
+  const [updateId, setUpdateId] = useState(null);
+
+  useEffect(() => {
+    dispatch(fetchDataRequest());
+  }, [dispatch]);
+
+  const handleCreate = () => {
+    dispatch(createDataRequest({ title }));
+    setTitle('');
+  };
+
+  const handleUpdate = () => {
+    dispatch(updateDataRequest(updateId, { title }));
+    setTitle('');
+    setUpdateId(null);
+  };
+
+  const handleDelete = (id) => {
+    dispatch(deleteDataRequest(id));
+  };
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
+  if (error) {
+    return <Text>Error: {error}</Text>;
+  }
+
+  return (
+    <ScrollView style ={{width:"100%"}}>
+      <TextInput
+        placeholder="Nhap  de tao"
+        value={title}
+        onChangeText={setTitle}
+        style={{ borderWidth: 1, marginBottom: 10, padding: 5 ,borderRadius:20}}
+      />
+      <Button
+        color="lightblue"
+        
+        title={updateId ? "Update" : "Create"}
+        onPress={updateId ? handleUpdate : handleCreate}
+      />
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10 }}>
+            <Text>{item.title}</Text>
+            <View style={{ flexDirection: 'row',gap:10 }}>
+              <Button color="green" title="Update" onPress={() => { setTitle(item.title); setUpdateId(item.id); }} />
+              <Button color="gray" title="Delete" onPress={() => handleDelete(item.id)} />
+            </View>
+          </View>
+        )}
+      />
+    </ScrollView>
+  );
+};
+
+const App = () => (
+  <Provider store={store}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <DataComponent />
+    </View>
+  </Provider>
+);
+
+export default App;
